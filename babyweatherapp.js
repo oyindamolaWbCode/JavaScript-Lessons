153942dbc708b24bfef5ed2e0e2fb76b64c361b6
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2023-11-21 02:54:33 +0100

    conditions and operators

---
+++ babyweatherapp.js
@@ -0,0 +1,12 @@
+//Conditionals
+//Conditionals are used to comapre variables values and/or data types. They always return true or false
+//Conditional operators
+// ==. ===, <, <=, >=, !=, !==
+
+let Weather = "rainy";
+
+if (Weather == "rainy") {
+  console.log("Grab your Umbrella ☔");
+} else {
+  console.log("Wear your sunglasses 😎");
+}
---
+++ lessonone.js
@@ -0,0 +1,40 @@
+console.log("oyindamola");
+
+// Variable is a placeholder to store infomation and data in your app
+
+name = "Oyindamola Kushimo";
+console.log(name);
+
+//Maths Operators
+
+// const NumberOne = Number(prompt("Give a number greater than 5"));
+
+// const Discount = 0.4;
+
+// const SumNumber = `${NumberOne + Discount}`;
+
+// const ProductNumber = `${NumberOne * Discount}`;
+// console.log(SumNumber);
+// alert(
+//   `The sum of food : ${NumberOne} and discount: ${Discount} is ${SumNumber}`
+// );
+
+// console.log(ProductNumber);
+
+//Maths Methods
+/*
+Math.floor() methods rounds a number down to the nearest integer, and returns the result.
+
+Math.ceil() methods rounds the number up to the nearest integer and returns the value
+
+Math.random() returns a random number between 09(inclusive ) and 1(exclusive)
+*/
+
+const figuredecimal = 500.2;
+const figureTwo = 30;
+
+const FigureTotalSum = Math.ceil(figuredecimal + figureTwo);
+console.log(FigureTotalSum);
+
+const FigureTotalProduct = Math.floor(figuredecimal * figureTwo);
+console.log(FigureTotalProduct);
